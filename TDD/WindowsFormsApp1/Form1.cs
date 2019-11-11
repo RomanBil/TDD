716cc90b84b8f0dc695be2b38a25cfca91e1b75a
Роман Білоус <mrbm@meta.ua>
2019-11-11 17:11:39 +0200

    test

--- TDD/WindowsFormsApp1/Form1.cs
+++ TDD/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TDDs;
 
 namespace WindowsFormsApp1
 {
@@ -19,7 +20,27 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            label6.Text = string.Empty;
+
+            label7.Text = string.Empty;
+
+            TDD tdd = new TDD();
+
+            int[] res = tdd.Work(Convert.ToInt32(textBox1.Text),
+                Convert.ToInt32(textBox2.Text),
+                Convert.ToInt32(textBox3.Text));
+
+            if (res != null)
+            {
+                label6.Text = res[0].ToString();
+
+                label7.Text = res[1].ToString();
+            }
+
+            else
+            {
+                MessageBox.Show("Error");
+            }
         }
     }
 }
