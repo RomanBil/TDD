92904b310f0170b3bdd3f95405bc5d828714a647
AndrewKalushka <andrew.kalushka@gmail.com>
2019-11-11 11:39:10 +0200

    test

--- TDD/TDD/TDD.cs
+++ TDD/TDD/TDD.cs
@@ -4,26 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TDD
+namespace TDDs
 {
     public class TDD
     {
         public int Discriminator(int a, int b, int c)
         {
-            return b * b - 4 * a * c;
+            return b * b - (4 * a * c);
         }
 
         public int[] Work(int a, int b, int c)
         {
+            //a*x*x+b*x+c=0
+
             int d = Discriminator(a, b, c);
 
             if (d >= 0)
             {
                 int[] res = new int[2];
 
-                res[0] = (-b + (int)Math.Sqrt(d) / (2 * a));
+                res[0] = (-b + (int)Math.Sqrt(d)) / (2 * a);
 
-                res[1] = (-b - (int)Math.Sqrt(d) / (2 * a));
+                res[1] = (-b - (int)Math.Sqrt(d)) / (2 * a);
 
                 return res;
             }
--- TDD/testTDD/test.cs
+++ TDD/testTDD/test.cs
@@ -4,16 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDDs;
 
 namespace testTDD
 {
     [TestFixture]
     public class test
     {
+        private TDD tdd = new TDD();
+
         [Test]
-        public void TestTdd()
+        public void TestDiscriminator()
         {
-            
+            int d = tdd.Discriminator(1, 2, 1);
+
+            Assert.AreEqual(0, d);
+        }
+
+        [Test]
+        public void TestWork()
+        {
+            int[] i = tdd.Work(1, 2, 1);
+
+            Assert.AreEqual(-1, i[0]);
+
+            Assert.AreEqual(-1, i[1]);
         }
     }
 }
