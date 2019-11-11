d3eda2f3545ba7936ab525bcd08374e08d949117
AndrewKalushka <andrew.kalushka@gmail.com>
2019-11-11 11:20:33 +0200

    test

--- TDD/TDD/TDD.cs
+++ TDD/TDD/TDD.cs
@@ -8,9 +8,30 @@ namespace TDD
 {
     public class TDD
     {
-        public void Work()
+        public int Discriminator(int a, int b, int c)
         {
+            return b * b - 4 * a * c;
+        }
 
+        public int[] Work(int a, int b, int c)
+        {
+            int d = Discriminator(a, b, c);
+
+            if (d >= 0)
+            {
+                int[] res = new int[2];
+
+                res[0] = (-b + (int)Math.Sqrt(d) / (2 * a));
+
+                res[1] = (-b - (int)Math.Sqrt(d) / (2 * a));
+
+                return res;
+            }
+
+            else
+            {
+                return null;
+            }
         }
     }
 }
--- TDD/testTDD/test.cs
+++ TDD/testTDD/test.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace testTDD
 {
+    [TestFixture]
     public class test
     {
-
+        [Test]
+        public void TestTdd()
+        {
+            
+        }
     }
 }
